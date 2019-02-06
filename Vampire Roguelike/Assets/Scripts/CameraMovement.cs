8dc59fe1f644503a622c655b6afa995a6f1ef3c7
Colin Landry <colin.landry01@stclairconnect.ca>
2019-02-06 17:07:59 -0500

    Camera Update
    - Camera now utilizes Mathf.Lerp functions to follow smoothly

---
+++ Vampire Roguelike/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (followTarget != null)
+        {
+            Vector3 p = transform.position;
+
+            p.x = Mathf.Lerp(transform.position.x, followTarget.transform.position.x, Time.deltaTime * moveSpeed);
+            p.y = Mathf.Lerp(transform.position.y, followTarget.transform.position.y, Time.deltaTime * moveSpeed);
+
+            transform.position = p;
+        }
+    }
+}
