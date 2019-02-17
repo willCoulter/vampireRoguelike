44dbf97bd4d11a08cb50f65197db5cb0f70a668e
willCoulter <will.coulter01@stclairconnect.ca>
2019-02-17 04:28:21 -0500

    #8 Added Basic attack feature in one direction

---
+++ Vampire Roguelike/Assets/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public float speed;
+    public SpriteRenderer sprite;
+
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Damage Taken");
+        sprite.color = Color.red;
+    }
+}
---
+++ Vampire Roguelike/Assets/Scripts/PlayerAttacks.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacks : MonoBehaviour
+{
+    private float attackLag;
+    public float startLag;
+
+    public float range;
+    public int damage;
+    public int finalHitDamage;
+    public Transform attackPos;
+    public LayerMask enemiesMask;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(attackLag <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos.position, range, enemiesMask);
+                for (int i = 0; i < enemies.Length; i++)
+                {
+                    enemies[i].GetComponent<Enemy>().takeDamage(damage);
+                }
+                attackLag = startLag;
+            }
+            
+        }
+        else
+        {
+            attackLag -= Time.deltaTime;
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, range);
+
+    }
+}
