79c70ffbe69eaa6cc51b3d2c1017a359ab288222
willCoulter <will.coulter01@stclairconnect.ca>
2019-02-17 06:41:00 -0500

    #8 Test Code in for moving the attacking hitbox

--- Vampire Roguelike/Assets/Scripts/PlayerAttacks.cs
+++ Vampire Roguelike/Assets/Scripts/PlayerAttacks.cs
@@ -10,7 +10,7 @@ public class PlayerAttacks : MonoBehaviour
     public float range;
     public int damage;
     public int finalHitDamage;
-    public Transform attackPos;
+    public GameObject attackPos;
     public LayerMask enemiesMask;
     
 
@@ -23,11 +23,12 @@ public class PlayerAttacks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        attackFacing();
         if(attackLag <= 0)
         {
             if (Input.GetKey(KeyCode.Mouse0))
             {
-                Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos.position, range, enemiesMask);
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos.transform.position, range, enemiesMask);
                 for (int i = 0; i < enemies.Length; i++)
                 {
                     enemies[i].GetComponent<Enemy>().takeDamage(damage);
@@ -44,7 +45,39 @@ public class PlayerAttacks : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, range);
+        Gizmos.DrawWireSphere(attackPos.transform.position, range);
 
+    }
+    private void attackFacing()
+    {
+        //Change orientation based on mouse location
+        Vector3 playerScreenPoint = transform.position;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouse = Input.mousePosition;
+        Vector3 convert = Camera.main.ScreenToViewportPoint(mouse);
+        Debug.Log(convert);
+        //Face Down
+        if (mousePos.y < playerScreenPoint.y && convert.y > convert.x)
+        {
+            attackPos.transform.localPosition = new Vector3(0, -0.63f, 0);
+        }
+        //Face Up
+        else if (mousePos.y > playerScreenPoint.y && convert.y > convert.x)
+        {
+            attackPos.transform.localPosition = new Vector3(0, 0.63f, 0);
+        }
+        //Face Left
+        else if (mousePos.x < playerScreenPoint.x)
+        {
+            attackPos.transform.localPosition = new Vector3(-0.63f, 0, 0);
+           // Debug.Log("Faced Left");
+        }
+        //Face Right
+        else if  (mousePos.x > playerScreenPoint.x)
+        {
+            attackPos.transform.localPosition = new Vector3(0.63f, 0, 0);
+            //Debug.Log("Faced Right");
+        }
+        
     }
 }
