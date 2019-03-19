44afff83eecdacb6482cf21cf68b33cffd20c08a
Colin Landry <colin.landry01@stclairconnect.ca>
2019-03-19 17:06:05 -0400

    Added Inventory framework #12
    * GameManager created to keep track of skill and item inventories
    * ItemInventory class and Item class created

---
+++ Vampire Roguelike/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : ScriptableObject
+{
+    public string name = "New Item";
+    public string desc = "New Desc";
+    public Sprite itemSprite;
+
+    public abstract void Initialize(GameObject target);
+
+}
---
+++ Vampire Roguelike/Assets/Scripts/Items/ItemInventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInventory : MonoBehaviour
+{
+
+    public static ItemInventory instance;
+    public int slots = 15;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one inventory instance");
+            return;
+        }
+
+        instance = this;
+    }
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if (items.Count >= slots)
+        {
+            Debug.Log("Not enough slots");
+            return false;
+        }
+
+        items.Add(item);
+        UIManager.instance.UpdateSkillSlot(items.Count);
+
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+}
