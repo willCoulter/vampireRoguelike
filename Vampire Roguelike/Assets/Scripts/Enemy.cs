47ddba0eaa4eff23d7f67435ddf3848b711ad049
Will Coulter <will.coulter01@stclairconnect.ca>
2019-03-25 20:11:10 -0400

    Fixed Bugs on enemies
    Fixed hitbox bug and enemy prefab errors

--- Vampire Roguelike/Assets/Scripts/Enemy.cs
+++ Vampire Roguelike/Assets/Scripts/Enemy.cs
@@ -16,28 +16,33 @@ public class Enemy : MonoBehaviour
 
     private Animator anim;
     public Room room;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //Grab an active player from the scene
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health <= 0)
+        //Check the enemies health
+        if (health <= 0)
         {
+            //If they have less than or equal to 0 health kill the enemy with the die function and reward the player with gold
             player.GetComponent<PlayerController>().gainGold(2);
             Die();
         }
     }
-
+    //Called by other gameobjects like the player accepts a damage amount of type float
     public void takeDamage(float damage)
     {
+        //Spawn a bloodparticle
         Instantiate(bloodParticle, transform.position, Quaternion.identity);
+        //Decrease the health of the enemy by the players damage
         health -= damage;
-        Debug.Log("Damage Taken");
         sprite.color = Color.red;
         player.GetComponent<PlayerController>().gainBlood(2);
     }
