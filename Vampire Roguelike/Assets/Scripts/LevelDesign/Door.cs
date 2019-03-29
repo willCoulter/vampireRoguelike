a25701918beea4d6282661935d7924177e5e6d63
Colin Landry <colin.landry01@stclairconnect.ca>
2019-03-29 19:05:46 -0400

    Door Update
    * Doors now change sprite based on locked

--- Vampire Roguelike/Assets/Scripts/LevelDesign/Door.cs
+++ Vampire Roguelike/Assets/Scripts/LevelDesign/Door.cs
@@ -7,10 +7,14 @@ public class Door : MonoBehaviour
     BoxCollider2D doorCollider;
 
     bool isLocked;
+    public Sprite closedSprite;
+    public Sprite openSprite;
+    SpriteRenderer sr;
 
     private void Start()
     {
         isLocked = false;
+        sr = gameObject.GetComponent<SpriteRenderer>();
 
         //If there is an attached door, grab box collider
         if(gameObject != null){
@@ -23,9 +27,11 @@ public class Door : MonoBehaviour
         //If door locked, enable collider
         if(isLocked){
             doorCollider.enabled = true;
+            sr.sprite = closedSprite;
         //If not locked and enabled, disable collider
         }else if(doorCollider.enabled == true && isLocked == false){
             doorCollider.enabled = false;
+            sr.sprite = openSprite;
         }
     }
 
