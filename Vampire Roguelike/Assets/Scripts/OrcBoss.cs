bc3cfbbb2e1b536de461346398f5b2352bad7919
Will Coulter <will.coulter01@stclairconnect.ca>
2019-04-11 12:31:45 -0400

    Enemy Melee system is finished

--- Vampire Roguelike/Assets/Scripts/OrcBoss.cs
+++ Vampire Roguelike/Assets/Scripts/OrcBoss.cs
@@ -7,54 +7,56 @@ public class OrcBoss : Enemy
     public Transform weapon;
     private float attackLag;
     public float startLag;
-    private bool attacking;
+    private bool attacking = false;
     public float slamDamage;
     public float slamCooldown;
     private float cooldownCounter;
     public float slamRadius;
     public GameObject slam;
+    public GameObject swordPrefab;
+    public Animator swordAnim;
 
     private void FixedUpdate()
     {
-        NormalAttack();
         AttackFacing();
+        NormalAttack();
+        
         SlamAttack();
     }
 
 
     private void AttackFacing()
     {
-        if (attacking != true)
-        {
+        if (attacking == false) {
             //grabs the differnce in the swords positions and the mouses
             Vector2 direction = new Vector2(
              target.transform.position.x - transform.position.x,
              target.transform.position.y - transform.position.y
              );
             //updates the swords transfrom from the right
-            weapon.transform.up = direction;
+            swordPrefab.transform.right = direction;
         }
-
     }
 
     private void NormalAttack()
     {
-        if (attackLag <= 0)
+        if (attacking == false)
         {
-            float distance = Vector3.Distance(target.position, transform.position);
-            
-            if (distance <= miniumRange)
+            if (attackLag <= 0)
             {
+                float distance = Vector3.Distance(target.position, transform.position);
 
-                attacking = true;
-                //weapon.transform.Rotate(new Vector3(0, 0, -180));
-                anim.SetTrigger("Attack");
-                attackLag = startLag;
+                if (distance <= miniumRange)
+                {
+                    //GameObject sword = Instantiate(swordPrefab, weapon.position, weapon.rotation,weapon.transform);
+                    StartCoroutine(AttackCo());
+
+                }
             }
-        }
-        else
-        {
-            attackLag -= Time.deltaTime;
+            else
+            {
+                attackLag -= Time.deltaTime;
+            }
         }
     }
 
@@ -85,6 +87,20 @@ public class OrcBoss : Enemy
             cooldownCounter -= Time.deltaTime;
         }
     }
+
+    public IEnumerator AttackCo()
+    {
+        swordPrefab.SetActive(true);
+        attacking = true;
+        swordAnim.SetBool("swing", true);
+        yield return new WaitForSeconds(0.5f);
+        swordAnim.SetBool("swing", false);
+        attacking = false;
+        swordPrefab.SetActive(false);
+        attackLag = startLag;
+
+    }
+
 }
 
 
---
+++ Vampire Roguelike/Assets/swordHit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordHit : MonoBehaviour
+{
+    public float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("oof");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().takeDamage(damage);
+        }
+    }
+}
