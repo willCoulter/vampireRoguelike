2adf9244a0ad59be85c456738a42ee305f7e74a7
Colin Landry <colin.landry01@stclairconnect.ca>
2019-04-11 22:50:25 -0400

    Started Level 3
    * Started laying out rooms for level 3
    * Copied UI manager and canvas to level 3
    * Edited level 3 tilemap

--- Vampire Roguelike/Assets/Scripts/GameManager.cs
+++ Vampire Roguelike/Assets/Scripts/GameManager.cs
@@ -94,6 +94,7 @@ public class GameManager : MonoBehaviour
                 break;
         }
 
+        GameManager.instance.itemsGathered = 0;
         UIManager.instance.RefreshSkillSlots();
     }
 
